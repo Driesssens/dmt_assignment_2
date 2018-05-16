0e8644328ad1bc2a40c5054baaf1b2eb4ff9a852
thomasdegroot18 <thomasdegroot18@gmail.com>
2018-05-17 01:00:39 +0200

    experiments run test

--- base_experiment.py
+++ base_experiment.py
@@ -3,7 +3,7 @@ from configuration import make_configuration
 
 
 class BaseExperiment(AbstractExperiment):
-    split_identifier = "spl_20180511180820"
+    split_identifier = "spl_20180516092856"
 
     experiment_name = "BaseExperiment"
     experiment_description = """The most basic experiment. Ignores 'date_time', includes all other raw features without doing any preprocessing. Uses 0.000 for missing values."""
@@ -17,7 +17,13 @@ class BaseExperiment(AbstractExperiment):
         return raw_data_frame
 
 
-BaseExperiment().run_mini_experiment()
+#removing file for generating new data
+try:
+    shutil.rmtree('D:/Users/Thomas/Documents/GitHub/dmt_assignment_2/data/spl_20180516092856/BaseExperiment/development')
+except:
+    pass
 
-short_experiment = make_configuration(epochs=10)
-BaseExperiment().run_development_experiment(configuration=short_experiment, reset_data=True)
+BaseExperiment().run_medium_experiment(reset_data=True)
+
+# short_experiment = make_configuration(epochs=10)
+# BaseExperiment().run_development_experiment(configuration=short_experiment, reset_data=True)
