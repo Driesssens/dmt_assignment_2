4a1ee0aa26a9580e44a9800c13d2013888e4fcf7
thomasdegroot18 <thomasdegroot18@gmail.com>
2018-05-27 01:40:51 +0200

    Push Alexander data

--- final_experiments.py
+++ final_experiments.py
@@ -1,8 +1,8 @@
 from top_hustinx import HustinxExperimentTop
-
+from deGroot2_experiment import DeGroot2Experiment
 
 def run_final_experiments():
-    HustinxExperimentTop().run_final_experiment(missing_values_old_style=True)
+    DeGroot2Experiment().run_final_experiment(missing_values_old_style=True)
 
 
 run_final_experiments()
