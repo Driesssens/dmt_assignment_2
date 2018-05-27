1079a2e82536c2b615efb3499742ab2aabcdcd9a
thomasdegroot18 <thomasdegroot18@gmail.com>
2018-05-27 17:52:11 +0200

    Push plots and final results

---
+++ exploratory_data_analysis/accuracy.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+import numpy
+
+acc_tr_super = []
+acc_val_super = []
+acc_tr_base = []
+acc_val_base = []
+
+with open("Superrun_full_20180526060437/" + 'training_performance_per_epoch.txt','r') as model_fn:
+    for line in model_fn: 
+        line = line.split("[")[1].split("]")[0].split(", ") #or some other preprocessing
+        for number in line:
+        	acc_tr_super.append(float(number)) #storing everything in memory!
+
+with open("Superrun_full_20180526060437/" + 'validation_performance_per_epoch.txt','r') as model_fn:
+    for line in model_fn: 
+        line = line.split("[")[1].split("]")[0].split(", ") #or some other preprocessing
+        for number in line:
+        	acc_val_super.append(float(number)) #storing everything in memory!
+
+
+with open("Base_run_full_20180521120423/" + 'training_performance_per_epoch.txt','r') as model_fn:
+    for line in model_fn: 
+        line = line.split("[")[1].split("]")[0].split(", ") #or some other preprocessing
+        for number in line:
+        	acc_tr_base.append(float(number)) #storing everything in memory!
+
+
+with open("Base_run_full_20180521120423/" + 'validation_performance_per_epoch.txt','r') as model_fn:
+    for line in model_fn: 
+        line = line.split("[")[1].split("]")[0].split(", ") #or some other preprocessing
+        for number in line:
+        	acc_val_base.append(float(number)) #storing everything in memory!
+
+
+# summarize history for accuracy
+plt.plot(acc_tr_super[:-50])
+plt.plot(acc_val_super[:-50])
+plt.plot(acc_tr_base[:-50])
+plt.plot(acc_val_base[:-50])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['acc_tr_super', 'acc_val_super', 'acc_tr_base', 'acc_val_base'], loc='upper left')
+plt.show()
+
+plt.savefig('output/acc_plot.png', bbox_inches='tight')
+plt.clf()
+
+# # summarize history for loss
+# plt.plot(history.history['loss'])
+# plt.plot(history.history['val_loss'])
+# plt.title('model loss')
+# plt.ylabel('loss')
+# plt.xlabel('epoch')
+# plt.legend(['train', 'test'], loc='upper left')
+# plt.show()
