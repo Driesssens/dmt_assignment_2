63dafefd088a26db6b2705ff122657a6039b0b5e
LAPTOP-AHIPR-HU\alexander <alexanderhustinx@gmail.com>
2018-05-27 17:55:35 +0200

    Added plots that are in report

--- freq_statistics.py
+++ freq_statistics.py
@@ -93,7 +93,72 @@ dataset_click_size = dataset_click.shape[0]
 
 
 
+#plots click positions1
+N = 41
+click = get_freq_statistics('position', filter_missing_values=True, normalize=True, percentage=True)[0].tolist()
+click = [0] + click
 
+ind = np.arange(N)  # the x locations for the groups
+width = 0.30       # the width of the bars
 
+fig, ax = plt.subplots()
+
+rects1 = ax.bar(ind, click, width, color='b')
+
+# add some text for labels, title and axes ticks
+ax.set_ylabel('% clicked')
+ax.set_title('Amount clicked per position')
+#ax.set_xticks(ind + width / 2)
+ax.set_xticklabels((0,0,5,10,15,20,25,30,35,40))
+
+#ax.legend((rects1[0], rects2[0]), ('Booked', 'Clicked'))
+plt.savefig('click_position.png', bbox_inches='tight')
+plt.show()
+
+#plots 0,0.1,...,1.0 occurance of ranges of prop_location_score2
+N = 11
+book = (24.92, 30.53, 17.68, 10.09, 6.51, 3.99, 2.65, 1.79, 1.03, 0.57, 0.25)
+click = (28.02, 29.29, 16.71, 9.79, 6.24, 3.92, 2.61, 1.69, 0.98, 0.54, 0.21)
+
+ind = np.arange(N)  # the x locations for the groups
+width = 0.25       # the width of the bars
+
+fig, ax = plt.subplots()
+
+rects1 = ax.bar(ind, book, width, color='b')
+rects2 = ax.bar(ind + width, click, width, color='y')
+
+# add some text for labels, title and axes ticks
+ax.set_ylabel('Percentage')
+ax.set_title('Occurrence of rounded prop_location_score2')
+ax.set_xticks(ind + width / 2)
+ax.set_xticklabels((0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0))
+
+ax.legend((rects1[0], rects2[0]), ('Booked', 'Clicked'))
+plt.savefig('rounded_prop_loc_score2_occurrence.png', bbox_inches='tight')
+plt.show()
+
+#plots occurance of missing values and non missing values of prop_location_score2 in dataset, clicked dataset and booked dataset
+N = 3
+non_miss = (78.01, 89.54, 87.98)
+miss = (21.99, 10.46, 12.02)
+
+ind = np.arange(N)  # the x locations for the groups
+width = 0.35       # the width of the bars
+
+fig, ax = plt.subplots()
+
+rects1 = ax.bar(ind, non_miss, width, color='b')
+rects2 = ax.bar(ind + width, miss, width, color='r')
+
+# add some text for labels, title and axes ticks
+ax.set_ylabel('Percentage')
+ax.set_title('Occurrence prop_location_score2')
+ax.set_xticks(ind + width / 2)
+ax.set_xticklabels(('All data', 'Booked', 'Clicked'))
+
+ax.legend((rects1[0], rects2[0]), ('Non-missing', 'Missing'))
+plt.savefig('prop_loc_score2_occurrence.png', bbox_inches='tight')
+plt.show()
 
 
